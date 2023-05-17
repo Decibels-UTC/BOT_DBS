1e1b532f6f66070b517bffb8e6677e34dbedccb3
Adrien Bigotte <adr4500@gmail.com>
2023-05-17 02:27:45 +0200

    fix bugs

--- rename.py
+++ rename.py
@@ -6,10 +6,12 @@ from datetime import datetime
 import pymysql as MySQLdb
 from enum import Enum
 import asyncio
+import random as rd
 
 load_dotenv(find_dotenv())
 
 intents = discord.Intents.default()
+intents.members = True
 client = commands.Bot(
     command_prefix="!",
     intents=intents,
@@ -18,7 +20,9 @@ client = commands.Bot(
 intents.members = True
 intents.message_content = True
 
-field = Enum("field", ["id", "member_id", "original_username", "guild_id", "date"])
+field = Enum(
+    "field", ["id", "member_id", "original_username", "guild_id", "date"], start=0
+)
 db = MySQLdb.connect(
     host=os.environ["DB_HOST"],
     user=os.environ["DB_USER"],
@@ -31,39 +35,41 @@ async def restore_username_routine():
     # Define database fields
     global field
     global db
-    last_count = 0
+    last_count = -1
     while True:
         now = datetime.now()
         # Fetch all nickname change from database
         cursor = db.cursor()
         query = "SELECT COUNT(*) FROM user_rename"
         cursor.execute(query)
-        tab = cursor.fetchall()
+        tab_2 = cursor.fetchall()
 
-        if tab[0][0] != last_count:
-            last_count = int(tab[0][0])
+        if tab_2[0][0] != last_count:
+            last_count = int(tab_2[0][0])
             query = "SELECT * FROM user_rename"
             cursor.execute(query)
             tab = cursor.fetchall()
-            for row in tab:
-                # Parse database datetime into python datetime
-                date = datetime.strptime(row[field.date.value], "%Y-%m-%d %H:%M:%S.%f")
-                # If 24h has passed since the nickname change
-                if (now - date).days >= 1:
-                    # Restore the nickname
-                    guild = client.get_guild(int(row[field.guild_id.value]))
-                    member = guild.get_member(int(row[field.member_id.value]))
-                    await member.edit(nick=row[field.original_username.value])
-                    # Delete the nickname change from database
-                    query = f"DELETE FROM user_rename WHERE id = {row[field.id.value]}"
-                    cursor.execute(query)
-                    db.commit()
-        asyncio.sleep(300)
+        for row in tab:
+            # Parse database datetime into python datetime
+            date = row[field.date.value]
+            # If 24h has passed since the nickname change
+            if (now - date).days >= 1:
+                # Restore the nickname
+                guild = client.get_guild(int(row[field.guild_id.value]))
+                await guild.fetch_member(int(row[field.member_id.value]))
+                member = guild.get_member(int(row[field.member_id.value]))
+                await member.edit(nick=row[field.original_username.value])
+                # Delete the nickname change from database
+                query = f"DELETE FROM user_rename WHERE id = {row[field.id.value]}"
+                cursor.execute(query)
+                db.commit()
+        await asyncio.sleep(300)
 
 
 @client.event
 async def on_ready():
     print(f"Connecté en tant que {client.user} ! ")
+    client.loop.create_task(restore_username_routine())
 
 
 @client.event
@@ -78,51 +84,95 @@ async def on_message(message):
     phrase = " ".join(words)  # On transforme les mots en une phrase
 
     if "je suis" in phrase:
-        if message.author.id != 463652129878573056:  # if pas Cesar
-            index = words.index("je") + 1
-            try:
-                if words[index] == "suis":
-                    # Récupérer les mot suivant et l'envoyer en réponse
-                    t = " ".join(words[index + 1 :])
-
-                    member = message.author
-                    await member.edit(nick=t)
-
-                    response = f"Le pseudo de {member.mention} vient dêtre modifié !"
-                    await message.channel.send(response)
-                    # Get the current datetime
-                    now = datetime.now()
-                    formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
-                    # Add nickname change to database
-                    cursor = db.cursor()
-                    query = f"INSERT INTO user_rename (member_id, original_username, guild_id, date) VALUES ({None}, {member.id}, {member.name}, {message.guild.id}, {formatted_date})"
-                    cursor.execute(query)
-                    db.commit()
-            except:
+        if rd.randint(0, 0) < 1:
+            if message.author.id != 463652129878573056:  # if pas Cesar
+                index = words.index("je") + 1
                 try:
-                    t = " ".join(words[index + 1 : index + 2])
-                    member = message.author
-                    await member.edit(nick=t)
+                    if words[index] == "suis":
+                        # Récupérer les mot suivant et l'envoyer en réponse
+                        t = " ".join(words[index + 1 :])
 
-                    response = (
-                        f"Le pseudo de {member.mention} vient dêtre modifié !\cheh"
-                    )
-                    await message.channel.send(response)
-                    # Get the current datetime
-                    now = datetime.now()
-                    formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
-                    # Add nickname change to database
-                    cursor = db.cursor()
-                    query = f"INSERT INTO user_rename (member_id, original_username, guild_id, date) VALUES ({None}, {member.id}, {member.name}, {message.guild.id}, {formatted_date})"
-                    cursor.execute(query)
-                    db.commit()
-
+                        member = message.author
+                        former_name = member.display_name
+                        await member.edit(nick=t)
+                        response = (
+                            f"Le pseudo de {member.mention} vient dêtre modifié !"
+                        )
+                        await message.channel.send(response)
+                        # Get the current datetime
+                        now = datetime.now()
+                        formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
+                        # If user already in database
+                        cursor = db.cursor()
+                        query = (
+                            f"SELECT * FROM user_rename WHERE member_id = {member.id}"
+                        )
+                        cursor.execute(query)
+                        tab = cursor.fetchall()
+                        if len(tab) > 0:
+                            # Update nickname change in database
+                            query = f"UPDATE user_rename SET date = '{formatted_date}' WHERE member_id = {member.id}"
+                            cursor.execute(query)
+                            db.commit()
+                        else:
+                            # Add nickname change to database
+                            cursor = db.cursor()
+                            parameters = (
+                                None,
+                                member.id,
+                                former_name,
+                                message.guild.id,
+                                formatted_date,
+                            )
+                            query = "INSERT INTO user_rename VALUES(%s, %s, %s, %s, %s)"
+                            cursor.execute(query, parameters)
+                            db.commit()
                 except:
-                    await message.channel.send(f"Bien esquivé  {member.mention}")
-        else:
-            await message.channel.send(
-                f"Il y en a qui ont des passe droits ! {member.mention}"
-            )
+                    try:
+                        t = " ".join(words[index + 1 : index + 2])
+                        member = message.author
+                        former_name = member.display_name
+                        await member.edit(nick=t)
+
+                        response = (
+                            f"Le pseudo de {member.mention} vient dêtre modifié !\cheh"
+                        )
+                        await message.channel.send(response)
+                        # Get the current datetime
+                        now = datetime.now()
+                        formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
+                        # If user already in database
+                        cursor = db.cursor()
+                        query = (
+                            f"SELECT * FROM user_rename WHERE member_id = {member.id}"
+                        )
+                        cursor.execute(query)
+                        tab = cursor.fetchall()
+                        if len(tab) > 0:
+                            # Update nickname change in database
+                            query = f"UPDATE user_rename SET date = '{formatted_date}' WHERE member_id = {member.id}"
+                            cursor.execute(query)
+                            db.commit()
+                        else:
+                            # Add nickname change to database
+                            cursor = db.cursor()
+                            parameters = (
+                                None,
+                                member.id,
+                                former_name,
+                                message.guild.id,
+                                formatted_date,
+                            )
+                            query = "INSERT INTO user_rename VALUES(%s, %s, %s, %s, %s)"
+                            cursor.execute(query, parameters)
+                            db.commit()
+
+                    except:
+                        await message.channel.send(f"Bien esquivé  {member.mention}")
+            else:
+                await message.channel.send(
+                    f"Il y en a qui ont des passe droits ! {member.mention}"
+                )
 
 
 # Remplacez le TOKEN avec votre propre token de bot Discord
